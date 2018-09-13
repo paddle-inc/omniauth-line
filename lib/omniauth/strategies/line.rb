0e01af9ef6beb96c6d1c718bcac619ed5b6321a1
Kazuki Kobashi <>
2018-09-13 15:49:41 +0900

    add response

--- lib/omniauth/strategies/line.rb
+++ lib/omniauth/strategies/line.rb
@@ -25,7 +25,7 @@ module OmniAuth
         {
           name:        raw_info['displayName'],
           image:       raw_info['pictureUrl'],
-          description: raw_info['statusMessage']
+          description: access_token.response          
         }
       end
 
